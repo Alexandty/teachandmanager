2579a1eafe0055b18db2f959ae13816918966bf6
Mateo <mateo.betancur@techandsolve.com>
2018-07-17 16:09:02 -0500

    Front componentes

--- my-request/src/App.js
+++ my-request/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Banner from './Components/Banner';
 
@@ -7,11 +9,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Banner />
+
+        <AppRouter />
 
       </div>
     );
   }
 }
+
+const AppRouter = () => (
+  <Switch>
+    <Route exact path="/" component={Login} />
+    <Route path="/Consulta" component={Consulta} />
+  </Switch>
+);
+
+const Consulta = () => (
+  <Jumbotron>
+    <h1>Consulta</h1>
+    {/* < /> */}
+  </Jumbotron>
+);
+
+const Login = () => (
+  <Jumbotron>
+    <h1>Login</h1>
+    {/* < /> */}
+  </Jumbotron>
+);
 
 export default App;
---
+++ my-request/src/Components/Input.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning },
+  readonly
+}) => (
+  <div className="form-group m-l">
+    <label className="text-white" htmlFor={input.id}>
+      {label}
+    </label>
+    <br />
+    <input
+      {...input}
+      type={type}
+      className="form-control"
+      disabled={readonly === true}
+    />
+    {touched &&
+      ((error && <span className="error">{error}</span>) ||
+        (warning && <span>{warning}</span>))}
+  </div>
+);
+
+export const Radio = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning },
+  readonly
+}) => (
+  <div className="form-group m-l">
+    <label className="text-white" htmlFor={input.id}>
+      {label}
+    </label>
+    <br />
+    <input
+      {...input}
+      className="input-rd option-input radio"
+      type={type}
+      disabled={readonly === true}
+    />
+    {touched &&
+      ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+  </div>
+);
