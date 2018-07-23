c9e81a3bf5a3cdf33a4356d1a9a888960c4a5f8d
joxelito950 <jose-950@hotmail.com>
2018-07-23 15:06:23 -0500

    test app passed

--- my-request/src/App.test.js
+++ my-request/src/App.test.js
@@ -1,9 +1,13 @@
+// setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
