d544ab9fcef0d609080beb1c5812efb3abcd0634
alexander <alexander.marquez@techandsolve.com>
2018-07-27 16:53:01 -0500

    ordenando listas de la tabla

--- my-request/src/Components/ProductList.js
+++ my-request/src/Components/ProductList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Table } from 'react-bootstrap';
+import { Table, Label } from 'react-bootstrap';
 
 
 const RequestVacationList = ({VacationData, user}) => {
@@ -8,7 +8,7 @@ const RequestVacationList = ({VacationData, user}) => {
   
   return (
     <div>
-     {user.name} {user.lastName}
+     {user.name} {user.lastName} <Label bsStyle="success">fecha de ingreso a la empresa{user.entryDate}</Label>
       <Table striped bordered condensed hover>
         <thead>
           <tr>
@@ -20,6 +20,11 @@ const RequestVacationList = ({VacationData, user}) => {
 
         <tbody>
           {VacationData
+          .sort((productA,productB) => {
+            if(productA===productB) return 1
+            else if(productA < productB) return -0
+            else return 0
+          })
           .map(product =>
             <tr key={product.idRequest}>
               <td>{product.startDate}</td>
