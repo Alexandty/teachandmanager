33af9847aae75c64ea10ad60d06bb5c1eed03438
joxelito950 <jose-950@hotmail.com>
2018-08-08 08:25:45 -0500

    correcion test unitario requestVacation

--- my-request/src/Container/VacationsRequestList/__tests__/RequestVacationList.test.js
+++ my-request/src/Container/VacationsRequestList/__tests__/RequestVacationList.test.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
-import configureStore from 'redux-mock-store';
 import { RequestVacationList } from '../index';
-import ConnectedRequestVacationList from '../index';
 import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-const mockStore = configureStore();
 describe('test produclist', () => {
     it('renders  productos when store is empty', () => {
         const wrapper = shallow(<RequestVacationList loadRequestVacation={jest.fn()} VacationData={[{ idRequest: 1 }]} user={[{ 'name': 's' }]} />);
@@ -16,9 +13,8 @@ describe('test produclist', () => {
     });
 
     it('No debe mostrar productos cuando store este vacio', () => {
-        const store = mockStore({});
-        const wrapper = shallow(<ConnectedRequestVacationList store={store} />);
-        expect(wrapper.find("ListRequestVacationEmpty").length).toBe(1);
+        const wrapper = shallow(<RequestVacationList loadRequestVacation={jest.fn()} VacationData={[]} user={{ name: 'juan' }} />);
+        expect(wrapper.find(".ListRequestVacationEmpty").length).toBe(1);
     })
 
     it('Validando toMatchSnapshot RequestVacationList', () => {
