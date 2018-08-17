10050bac1188ac4ef4df96a68d6676f8621c7185
Mateo <slash-252@hotmail.com>
2018-08-17 15:58:13 -0500

    tests front

--- my-request/src/Container/SolicitudVacaciones/__tests__/reducer.test.js
+++ my-request/src/Container/SolicitudVacaciones/__tests__/reducer.test.js
@@ -6,16 +6,20 @@ describe('Test del reducer ADD_SOLICITUD_VACACIONES', () => {
         expect(AddSoliitudVacaciones([], "")).toEqual([]);
     })
     it('Debe ingresar al type ADD_SOLICITUD_VACACIONES', () => {
-        const action = { type: "ADD_SOLICITUD_VACACIONES", vacationSolicitudData:[ "yo"] }
-        expect(AddSoliitudVacaciones([], action)).toEqual({ "vacationSolicitudData":[ "yo"] });
+        const action = { type: "ADD_SOLICITUD_VACACIONES", vacationSolicitudData: ["yo"] }
+        expect(AddSoliitudVacaciones([], action)).toEqual({ "vacationSolicitudData": ["yo"] });
     })
     it('Debe ingresar al type CHECK_SOLICITUD_VACACIONES', () => {
-        const action = { type: "CHECK_SOLICITUD_VACACIONES", vacationSolicitudData:[ "yo"] }
-        expect(AddSoliitudVacaciones([], action)).toEqual({ "vacationSolicitudData":[ "yo"], 'availableDaysVacation': false });
+        const action = { type: "CHECK_SOLICITUD_VACACIONES", availableDaysVacation: 1, days: 1 }
+        expect(AddSoliitudVacaciones([], action)).toEqual({ "mensaje": "1 Dias disponibles para fecha seleccionada", 'availableDaysVacation': false });
     })
     it('Debe ingresar al type GET_AVAILABLE_DAYS', () => {
-        const action = { type: "GET_AVAILABLE_DAYS", avalableDaysData:1 }
-        expect(AddSoliitudVacaciones([], action)).toEqual({ "avalableDaysData":1});
+        const action = { type: "GET_AVAILABLE_DAYS", avalableDaysData: 1 }
+        expect(AddSoliitudVacaciones([], action)).toEqual({ "avalableDaysData": 1 });
+    })
+    it('Debe ingresar al type ERROR_SOLICITUD_VACACIONES', () => {
+        const action = { type: "GET_AVAILABLE_DAYS", avalableDaysData: 1 }
+        expect(AddSoliitudVacaciones([], action)).toEqual({ "avalableDaysData": 1 });
     })
 
 
