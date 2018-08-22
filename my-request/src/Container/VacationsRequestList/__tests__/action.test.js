5dd0680324a0159dbbaeeb429a066283475d973d
Mateo <slash-252@hotmail.com>
2018-08-22 09:19:10 -0500

    testing front

--- my-request/src/Container/VacationsRequestList/__tests__/action.test.js
+++ my-request/src/Container/VacationsRequestList/__tests__/action.test.js
@@ -1,17 +1,16 @@
 import axios from 'axios'
 jest.mock('axios')
-import action from './../action';
+import { loadRequestVacation } from './../action';
 
 describe('Testing VacationRequestList action', () => {
-    it('should call loadRequestVacation resolve', async () => {
+    it('should call loadRequestVacation and resolve', async () => {
         const dispatch = jest.fn();
         const user = 'JJ';
-        axios.get.mockReturnValue(new Promise((resolve) => resolve(values)))
+        axios.get.mockReturnValue(new Promise((resolve) => resolve(user)))
 
-        await action.consultar(values)(dispatch);
+        await loadRequestVacation(user)(dispatch);
 
-        expect(axios.get).toHaveBeenCalledWith("http://localhost:8081/solicitud/vacaciones/consultar/"
-        , values)
-
+        expect(axios.get).toHaveBeenCalledWith("http://localhost:8081/solicitud/vacaciones/consultar/" + user)
+        expect(dispatch).toHaveBeenCalledWith({ type: 'REPLACE_PRODUCTS' })
     });
 });
